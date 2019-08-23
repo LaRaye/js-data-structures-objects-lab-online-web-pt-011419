b118ff97bab726d2a9f1e44c16c82927b74e9420
LaRaye Johnson <lnj2105@gmail.com>
2019-08-23 20:01:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   
   return newDriver;
 }
+
+destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
